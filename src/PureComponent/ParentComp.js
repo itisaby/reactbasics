41e63c754ca18b100f8d7fd9bfb27a119cf6431f
itisaby <arnabbibhuti01@gmail.com>
2022-06-18 21:14:25 +0530

    Learnt ReactMemo it's like purecomp in functional

--- src/PureComponent/ParentComp.js
+++ src/PureComponent/ParentComp.js
@@ -1,10 +1,11 @@
 import React, { Component, PureComponent } from 'react'
+import MemoComp from '../Memo/MemoComp'
 import PureComp from './PureComp'
 import RegularComp from './RegularComp'
 
 // if it is made a pure component, it will not render again and again as it will check the previous and the current state and props and if they are same, it will not render again
 
-class ParentComp extends PureComponent {
+class ParentComp extends Component {
     constructor(props) {
       super(props)
     
@@ -23,8 +24,10 @@ class ParentComp extends PureComponent {
     console.log('ParentComp Rendered')
     return (
       <div>ParentComp
-        <RegularComp name={this.state.name} />
-        <PureComp name={this.state.name}/>
+        {/* <RegularComp name={this.state.name} />
+        <PureComp name={this.state.name}/> */}
+        {/* using memo in functional component */}
+        <MemoComp name={this.state.name}/>
       </div>
     )
   }
