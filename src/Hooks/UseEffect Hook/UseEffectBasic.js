948bc87951dea00a30b6fc5b98173b892858e5d8
Arnab Maity <arnabbibhuti01@gmail.com>
2022-06-21 08:19:55 +0000

    Conditional Rendering in useEffect(2nd parameter)

--- src/Hooks/UseEffect Hook/UseEffectBasic.js
+++ src/Hooks/UseEffect Hook/UseEffectBasic.js
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from 'react'
 
 function UseEffectBasic() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
     //useEffect is placed inside the component 
     useEffect(
         //when we specify useEffect we are basically requesting react to execute the function that is passed as an argument
         //useEffect runs after every render of the component
         ()=>{
+            console.log("Updating document title")
             document.title = `You clicked ${count} times`
-        }
+        },
+        //conditionally executing an effect we pass a second parameter
+        //array of values the effect is depend upon 
+        [count]
     )
   return (
     <div>
+        <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
         <button
             onClick={
                 ()=>{
